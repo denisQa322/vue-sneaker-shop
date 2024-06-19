3409945d12694f1392e1165a9ba41c3421f9d13e
denisQa322 <denis.isakov.99@gmail.com>
2024-06-20 00:17:38 +0500

    created cart

---
+++ src/components/CartItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+    <div class="flex flex-col">
+      <p>Кроссовки Nike Air Max 270</p>
+      <div class="flex justify-between mt-2">
+        <b>12990 руб.</b>
+        <img
+          class="opacity-40 hover:opacity-100 cursor-pointer transition"
+          src="/close.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/CartItemList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import CartItem from './CartItem.vue'
+</script>
+
+<template>
+  <div class="flex flex-col flex-1 gap-4">
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+  </div>
+</template>
